1b371b4a31c090b17faeda9852db0a2da8c0775a
Thakur <thakur@Thakurs-MacBook-Pro.local>
2023-12-28 17:02:12 +0530

    fixed css

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
